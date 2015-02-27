2dce28a5e551b08d9de6ad1d0de43adcc0d8ebf6
Malo Skrylevo <majioa@yandex.ru>
2015-02-27 18:53:36 +0300

    Added scope by_token for Author model, and spec.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ActiveRecord::Base
   has_and_belongs_to_many :languages, join_table: :languages_authors
+
+  scope :by_tokens, ->(tokens) { where name: tokens }
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   it { should have_and_belong_to_many :languages }
+
+  context "search by tokens" do
+    let( :author_attributes ) {
+      [ { name: "Name Lastname" }, { name: "Other Lastname" } ]
+    }
+
+    it "complex token" do
+      authors = Author.create author_attributes
+      tokens = [ 'Name Lastname' ]
+      expect( Author.by_tokens tokens ).to eq( [ authors[0] ] )
+    end
+  end
 end
