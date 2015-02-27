3774462778aafda32b2f46682128713a75304da8
Malo Skrylevo <majioa@yandex.ru>
2015-02-27 18:54:34 +0300

    Added scope by_token for LanguageType model, and spec.

--- app/models/language_type.rb
+++ app/models/language_type.rb
@@ -1,3 +1,5 @@
 class LanguageType < ActiveRecord::Base
   has_and_belongs_to_many :languages, join_table: :languages_language_types
+
+  scope :by_tokens, ->(tokens) { where name: tokens }
 end
--- spec/models/language_type_spec.rb
+++ spec/models/language_type_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe LanguageType, type: :model do
   it { should have_and_belong_to_many :languages }
+
+  context "search by tokens" do
+    let( :type_attributes ) {
+      [ { name: "Type" }, { name: "Type1" } ]
+    }
+
+    it "complex token" do
+      types = LanguageType.create type_attributes
+      tokens = [ 'Type' ]
+      expect( LanguageType.by_tokens tokens ).to eq( [ types[0] ] )
+    end
+  end
 end
